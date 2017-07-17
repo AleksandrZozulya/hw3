4393377deb8cd760af0d56b961a7ba9319725795
Aleksandr Zozulya <aleksandr.zozulya.1997@gmail.com>
2017-07-17 13:35:04 +0300

    hw3
    hw3

---
+++ const.js
@@ -0,0 +1,7 @@
+const INVITATION = "Welcome to the film database. Get started now?"; 
+const MENU = "Select the command digit:\n1. Add - 1\n2. Delete - 2\n3. Show base - 3\n4. Help - 4\n5. Exit - 5"; 
+const INPUT_FILM = "Enter film (Name and Date)."; 
+const INCORRECT_INPUT = "Incorrected data"; 
+const CM_NOT_FOUND = "Command not found"; 
+const CM_INFO =  "\n1. Add - 1\n2. Delete - 2\n3. Show base - 3\n4. Help - 4\n5. Exit - 5"
+const YEAR = 1992; 
---
+++ database.js
@@ -0,0 +1,9 @@
+var filmBase = [{
+                name: "Armageddon", year: 1998
+            }, {
+                name: "The Core", year: 2003
+            }, {
+                name: "The Matrix", year : 1999
+            }, {
+                name: "Ex Machina", year: 2015
+            }]; 
